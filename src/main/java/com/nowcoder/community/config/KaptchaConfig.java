8402417f12610212e8c2ada51c7636ba37fd4389
NyaWebDev <13703550951@163.com>
2021-05-02 11:56:35 +0800

    first commit

---
+++ src/main/java/com/nowcoder/community/config/KaptchaConfig.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community.config;
+
+import com.google.code.kaptcha.Producer;
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class KaptchaConfig {
+
+    @Bean
+    public Producer kaptchaProducer(){
+        Properties properties = new Properties();
+        //图片宽度
+        properties.setProperty("kaptcha.image.width", "100");
+        //图片高度
+        properties.setProperty("kaptcha.image.height", "40");
+        //字体大小
+        properties.setProperty("kaptcha.textproducer.font.size", "32");
+        //字体颜色
+        properties.setProperty("kaptcha.textproducer.font.color", "0,0,0");
+        //字符随机内容
+        properties.setProperty("kaptcha.textproducer.char.string", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        //字符随机数
+        properties.setProperty("kaptcha.textproducer.char.length", "4");
+        //图片防机器破解干扰类
+        properties.setProperty("kaptcha.noise.impl", "com.google.code.kaptcha.impl.NoNoise");
+
+        DefaultKaptcha kaptcha = new DefaultKaptcha();
+        Config config = new Config(properties);
+        kaptcha.setConfig(config);
+        return kaptcha;
+    }
+
+}
