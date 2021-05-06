cd8b54b9d3e8ad009a6cde1ad04ee40031fcdec0
NyaWebDev <13703550951@163.com>
2021-05-06 13:01:54 +0800

    first commit

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,11 +1,24 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.Date;
 
 @Service
 //@Scope("prototype")
@@ -14,22 +27,94 @@ public class AlphaService {
     @Autowired
     private AlphaDao alphaDao;
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
     public AlphaService(){
-        System.out.println("实例化AlphaService");
+        //System.out.println("实例化AlphaService");
     }
 
     @PostConstruct
     public void init(){
-        System.out.println("初始化AlphaService");
+        //System.out.println("初始化AlphaService");
     }
 
     @PreDestroy
     public void destroy(){
-        System.out.println("销毁AlphaService");
+        //System.out.println("销毁AlphaService");
     }
 
     public String find(){
         return alphaDao.select();
+    }
+
+    // REQUIRED: 支持当前事务(外部事物),如果不存在则创建新事物.
+    // REQUIRES_NEW: 创建一个新事物，并且暂停当前事务.
+    // NESTED: 如果当前存在事物(外部事物),则嵌套在该事务中执行
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1(){
+        // 新增用户
+        User user = new User();
+        user.setUsername("beta");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+        user.setEmail("beta@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 新增帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("你好");
+        post.setContent("新人报到beta!");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        // 故意报错，让事务回滚
+        Integer.valueOf("abc");
+
+
+        return "ok";
+    }
+
+
+    public Object save2(){
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus transactionStatus) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("alpha");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123"+user.getSalt()));
+                user.setEmail("alpha@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("Hello");
+                post.setContent("新人报到!");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                // 故意报错，让事务回滚
+                Integer.valueOf("abc");
+                return "ok";
+            }
+        });
     }
 
 }
