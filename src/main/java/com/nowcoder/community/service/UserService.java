55752e61f2eaaabe015040371900b05bfd68f311
NyaWebDev <13703550951@163.com>
2021-05-08 12:04:25 +0800

    first commit

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -209,4 +209,8 @@ public class UserService implements CommunityConstant {
         }
     }
 
+    public User findUserByName(String username){
+        return userMapper.selectByName(username);
+    }
+
 }
