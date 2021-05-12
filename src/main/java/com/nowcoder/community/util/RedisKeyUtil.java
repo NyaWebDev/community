8058b290c205c05c6be7360ad29f0a032ffd54f1
NyaWebDev <13703550951@163.com>
2021-05-12 13:44:46 +0800

    first commit

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,6 +5,9 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_ROLLOWEE = "followee";
+    private static final String PREFIX_ROLLOWER = "follower";
+
 
     // 某个实体的赞
     // like:entity"entityType:entityId -> set(userId)
@@ -16,6 +19,18 @@ public class RedisKeyUtil {
     // like:user:userId > int
     public static String getUserLikeKey(int userId){
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    // 某个用户关注的实体
+    // followee:userId:entityType -> zset(entityId,now)
+    public static String getFolloweeKey(int userId, int entityType){
+        return PREFIX_ROLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个用户拥有的粉丝
+    // follower:entityType:entityId -> zset(userId,now)
+    public static String getFollowerKey(int entityType, int entityId){
+        return PREFIX_ROLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 
 }
